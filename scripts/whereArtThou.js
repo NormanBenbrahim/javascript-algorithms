3a85537ede8029b47b65bf4cb01f21938e68d098
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-10 17:41:25 -0400

    fixed for last 2 debug conditions, working version

--- scripts/whereArtThou.js
+++ scripts/whereArtThou.js
@@ -21,26 +21,36 @@ function whereAreYou(collection, source) {
 	// Only change code below this line
 	// loop through the source object's key-value pairs one by one
 	var source_keys = Object.keys(source);
-  
-	for (i = 0; i<source_keys.length; i++) {
-        var current_property = source_keys[i];
-        var current_value = source[current_property];
-        
-      	for (j = 0; j<collection.length; j++) {
-			var current_object = collection[j];
-			
-			if (current_object.hasOwnProperty(current_property)) {
-				if (current_value===current_object[current_property]) {
-					arr.push(current_object);
-				}
-			}
-			
-		}
-	}
+  	
+  	for (i = 0; i<collection.length; i++) {
+  		// create a condition that must evaluate to true in order to add the object
+        // in other words we want both elements of the source j to be within
+        // the object i in order to push them to arr
+  		var push = true;
+  		
+  		var current_object = collection[i];
+  		
+  		for (j = 0; j<source_keys.length; j++) {
+  			var current_property = source_keys[j];
+  			var current_value = source[current_property];
+  			
+  			if (current_object.hasOwnProperty(current_property)!==true) {
+  				push = false;
+  			}
+  			else if (current_value!==current_object[current_property]) {
+  				push = false;
+  			}
+  		}
+  		
+  		if (push) {
+  			arr.push(current_object);
+  		}
+  	}
   
 	// Only change code above this line
 	return arr;
 }
+
 
 
 
