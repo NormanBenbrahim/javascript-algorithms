cfffbd9e61044063e421634daff1a2f798f27f64
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-11 16:52:46 -0400

    working solution. regular expressions are neat

--- scripts/spinalCase.js
+++ scripts/spinalCase.js
@@ -11,12 +11,28 @@ String.replace()
 */
 
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  return str;
+	// the first letter either gets lowercase or doesn't change, so we can
+	// omit it and worry about it later
+	var first_letter = str[0];
+	var str_to_match = str.slice(1);
+	
+	// create the regular expression condition
+	// we want to replace underscores and spaces with dashes 
+	var re = new RegExp('_| ', 'g');
+	var spinal = str_to_match.replace(re, '-');
+	
+	// if there were no underscores or spaces that means it was camel case or already 
+	// spinal case. either way we need to make sure with a second regexp
+	// neat thing: put groups in brackets to access them as 'variables', see below
+	spinal = spinal.replace(new RegExp(/([a-z])([A-Z])/, 'g'), "$1-$2");
+	spinal = spinal.toLowerCase();
+	
+	// add the first letter back
+	final = first_letter.toLowerCase() + spinal;
+	
+	// if it was separated
+	return final;
 }
-
-spinalCase('This Is Spinal Tap');
 
 /*
 
