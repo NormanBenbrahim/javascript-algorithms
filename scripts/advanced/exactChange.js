7c04da6ae3e4859734365d43856ca4c14c9f9597
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-17 12:23:07 -0400

    working for most of the decimal cases. this one's hard

--- scripts/advanced/exactChange.js
+++ scripts/advanced/exactChange.js
@@ -17,10 +17,159 @@ Here are some helpful links:
 Global Object
 */
 
+// I'm going to assume that nothing is over 100$ in the store, and that we accept 100$ bills
 function checkCashRegister(price, cash, cid) {
-  var change;
-  // Here is your change, ma'am.
-  return change;
+	var change = cash - price; // the change to be returned
+	var total_drawer = 0;
+	var decimal, n_quarters, n_dimes, n_pennies, n_nickels, n_5, n_10, n_20, n_50, n_100;
+	// build the cid object properly based on each denomination
+	// first simply store the values into an object
+	var drawer = {};
+	var return_drawer = {};
+	for (var i=0; i<cid.length; i++) {
+		var current_denomination = cid[i];
+		// first element is the key, second is the value
+		drawer[current_denomination[0]] = current_denomination[1];
+		return_drawer[current_denomination[0]] = 0;
+		// accumulate the total cash in the drawer
+		total_drawer += current_denomination[1];
+	}
+	
+	// a simple case is when we have less in the drawer than the change owed
+	if (total_drawer<change) {
+		return "Insufficient Funds";
+	}
+	
+	// convert from money in each denomination to amount in each denomination
+	drawer.PENNY = 100*drawer.PENNY;
+	drawer.NICKEL = (100/5)*drawer.NICKEL;
+	drawer.DIME = (100/10)*drawer.DIME;
+	drawer.QUARTER = (100/25)*drawer.QUARTER; 
+	
+	// check on the drawer
+	console.log('The drawer contains:');
+	console.log(drawer);
+	
+	// one obvious case is if you have only a penny, you can't return anything regardless
+	if (drawer.PENNY<=1 && drawer.NICKEL===0 && drawer.DIME===0 && drawer.QUARTER===0 && change>0.01) {
+		return 'Insufficient Funds';
+	}
+
+	               
+	// count down the change
+	while (change>0) {
+		// now handle the change situations
+		if (change>1.0) {
+			// there is a decimal amount to return
+			if (Math.floor(change) - change>0) {
+				decimal = Math.floor(change) - change;
+				decimal = 100*decimal;
+				
+				// if it's not divisible by 5, we got some pennies to give back
+				if (decimal % 5 !== 0) {
+					n_pennies = decimal % 5;
+					// easy case is that we don't have any pennies to give,
+					if (drawer.PENNY<n_pennies) {
+						return "Insufficient Funds";
+					}
+					else {
+						return_drawer.PENNY += n_pennies;
+						change -= 0.01*n_pennies;
+						drawer.PENNY -= n_pennies;
+						total_drawer -= 0.01*n_pennies;
+					}
+				}
+				// 3 quarters or more
+				else if (decimal>=75) {
+					n_quarters = 3;
+					// check that you have enough change in quarters first of all
+					if (drawer.QUARTER<3 && drawer.DIME<7 && drawer.NICKEL<15 && drawer.PENNY<75) {
+						return 'Insufficient Funds';
+					}
+					// easiest case is equality
+					else if (decimal===75) {
+						change -= 0.75;
+						drawer.QUARTER -= n_quarters;
+						total_drawer -= 0.75;
+					}
+					// if you have 75, 85 or 95 cents
+					else if (decimal % 10 !== 0) {
+						n_nickels = decimal % 10;
+						// we don't have any nickels
+						if (drawer.NICKELS<n_nickels) {
+							// we don't have any pennies
+							if (drawer.PENNIES<n_pennies) {
+								return 'Insufficient Funds';
+							}
+							// we have pennies to give back
+							else {
+								n_pennies = 5*n_nickels;
+								change -= 0.01*n_pennies;
+								drawer.PENNY -= n_pennies;
+								
+							}
+						}
+					}
+					// if you have 80 or 90 cents
+					
+				}
+				// 2 quarters or more
+				else if (decimal>=50 && decimal<75) {
+					n_quarters = 2;
+					// check that you have enough change in quarters at least
+					if (drawer.QUARTER<2 && drawer.DIME<5 && drawer.NICKEL<10 && drawer.PENNY<50) {
+						return 'Insufficient Funds';
+					}
+				}
+				// 1 quarter or more
+				else if (decimal<50 && decimal>=25) {
+					n_quarters = 1;
+					// check that you have enough change in quarters
+					if (drawer.QUARTER<1 && drawer.DIME<2 && drawer.NICKEL<5 && drawer.PENNY<25) {
+						return 'Insufficient Funds';
+					}
+				}
+				// less than one quarter
+				else {
+					// do stuff...
+				}
+				
+				/*
+				// if it's not divisible by 10, we got some nickels to give back
+				// we need to also ommit multiples of 25 such as 50 as we don't 
+				// want to give back 5 dimes if we don't have to
+				else if (decimal % 10 !== 0 && decimal%25!==0) {
+					n_nickels = decimal % 10;
+					// if we don't have any nickels or pennies, we ain't got the dough
+					if (drawer.NICKEL<n_nickels && drawer.PENNY<n_nickels) {
+						return 'Insufficient Funds';
+					}
+					else {
+						return_array.push(['NICKEl', 0.05*n_nickels]);
+						change -= 0.05*n_nickels;
+						drawer.NICKEL -= n_nickels
+						total_drawer -= 0.05*n_nickels;
+					}
+				}
+				// if it's divisible by 25, we got some quarters to give back
+				else if (decimal % 25 !== 0) {
+					n_quarters = 
+				}
+				*/
+			
+			// return a whole number
+			
+			}
+		// it is a number between 0.01 and 0.99
+		else {
+			
+		}		
+	
+	}
+
+	
+	return return_array;
+	}
 }
 
 /* --- Debug --- 
