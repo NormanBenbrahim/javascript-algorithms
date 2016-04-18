3103317bc6e4041f1ff8b1abd6dd2e7b1cb77e63
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-18 09:48:40 -0400

    working solution, needed help on this one

--- scripts/advanced/noRepeatsPlease.js
+++ scripts/advanced/noRepeatsPlease.js
@@ -15,7 +15,69 @@ RegExp
 */
 
 function permAlone(str) {
-	return str;
+	// the easiest case is all letters in str are the same, in which case return 0
+	var first = str[0];
+	var re = new RegExp('^' + first + '*' + '$');
+	if (str.match(re)!==null) {
+		return 0;
+	}
+	// otherwise there's work to do
+	
+	// initialize variables
+    var total = 0, firsts = "", repeats = "";	
+	
+	// first define some helper functions
+    // string permutator (after Filip Nguyen)
+    function stringPermutator(str) {
+        var fact = [1], permutations = [];
+        for (var i = 1; i <= str.length; i++) fact[i] = i * fact[i - 1];
+        for (i = 0; i < fact[str.length]; i++) {
+            var perm = "", temp = str, code = i;
+            for (var pos = str.length; pos > 0; pos--) {
+                var sel = code / fact[pos - 1];
+                perm += temp.charAt(sel);
+                code = code % fact[pos - 1];
+                temp = temp.substring(0, sel) + temp.substring(sel + 1);
+            }
+            permutations.push(perm);
+        }
+        return permutations;
+    }	
+	
+    // RECURSIVE CHARACTER INSERTER
+    function insertRepeats(firsts, repeats) {
+        var pos = -1;
+        for (var i = 0; i < firsts.length, pos < 0; i++) {
+            pos = repeats.indexOf(firsts.charAt(i));
+        }
+        var char = repeats.charAt(pos);
+        for (i = firsts.indexOf(char) + 2; i <= firsts.length; i++) {
+            var combi = firsts.slice(0, i) + char + firsts.slice(i);
+            if (repeats.length > 1) {
+                insertRepeats(combi, repeats.slice(0, pos) + repeats.slice(pos + 1));
+            } else {
+                ++total;
+            }
+        }
+    }	
+    
+    // the algorithm
+    for (var i = 0; i < str.length; i++) {
+        char = str[i];
+        if (str.indexOf(char) == i) {
+        	firsts += char;
+        }
+        else {
+        	repeats += char;
+        }
+    }
+    firsts = stringPermutator(firsts);
+    for (i = 0; i < firsts.length; i++) {
+        insertRepeats(firsts[i], repeats);
+    }
+    
+    return Math.pow(2, repeats.length) * total;
+
 }
 
 /* --- Debug ---
