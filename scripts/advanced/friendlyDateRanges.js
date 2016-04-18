f2330438b07d90757d5aad84a34d0eeda7eeef91
NormanBenbrahim <norman.benbrahim@gmail.com>
2016-04-18 11:54:11 -0400

    working solution

--- scripts/advanced/friendlyDateRanges.js
+++ scripts/advanced/friendlyDateRanges.js
@@ -28,7 +28,112 @@ parseInt()
 */
 
 function makeFriendlyDates(arr) {
-  return arr;
+	
+	var d1 = arr[0], d2 = arr[1];
+	
+	// note year 1 is always smaller than year 2, but make sure just in case
+	d1 = d1.split('-'), d2 = d2.split('-');
+	if (parseInt(d2[0])<parseInt(d1[0])) {
+		d1 = arr[1];
+		d2 = arr[0];
+	} 	
+	
+	var year1 = parseInt(d1[0]), month1 = parseInt(d1[1]), day1 = parseInt(d1[2]);
+	var year2 = parseInt(d2[0]), month2 = parseInt(d2[1]), day2 = parseInt(d2[2]);
+	var return_array = [];
+	
+	// handle the cases for the date. easiest one is when both are the exact same
+	if (year1===year2 && month1===month2 && day1===day2) {
+		return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]) + ', ' + d1[0]);
+	}
+	// another easy one is same year same month
+	else if (year1===year2 && month1===month2) {
+		return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]));
+		return_array.push(dayConvert(d2[2]));
+	}
+	// if the date ends in less than a year from when it begins, don't display end year
+	else if (year2 - year1 === 1) {
+		if (12 - month1 + month2 < 12) { // less than a year
+			
+			return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]));
+			return_array.push(monthConvert(d2[1]) + ' ' + dayConvert(d2[2]));
+		}
+		else if (12 - month1 + month2===12) { // exactly a year
+			// this means month1==month2, check that the days add up to at least a year
+			if (day2<day1) {
+				return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]) + ', ' + d1[0]);
+				return_array.push(monthConvert(d2[1]) + ' ' + dayConvert(d2[2]));
+			}
+			else {
+				return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]) + ', ' + d1[0]);
+				return_array.push(monthConvert(d2[1]) + ' ' + dayConvert(d2[2]) + ', ' + d2[0]);
+			}
+		}
+		
+	}
+	// more than a year apart
+	else if (year2 - year1 > 1) {
+		return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]) + ', ' + d1[0]);
+		return_array.push(monthConvert(d2[1]) + ' ' + dayConvert(d2[2]) + ', ' + d2[0]);
+	}
+	// less than a year apart
+	else {
+		return_array.push(monthConvert(d1[1]) + ' ' + dayConvert(d1[2]) + ', ' + d1[0]);
+		return_array.push(monthConvert(d2[1]) + ' ' + dayConvert(d2[2]));
+	}
+	
+	
+	
+	// helper function to convert months in numbers to actual months
+	function monthConvert(month) {
+		switch(month) {
+			case '01':
+				return 'January';
+			case '02':
+				return 'February';
+			case '03':
+				return 'March';
+			case '04':
+				return 'April';
+			case '05':
+				return 'May';
+			case '06':
+				return 'June';
+			case '07':
+				return 'July';
+			case '08':
+				return 'August';
+			case '09':
+				return 'September';
+			case '10':
+				return 'October';
+			case '11':
+				return 'November';
+			case '12':
+				return 'December';
+		}
+	}
+	
+	// helper function to convert the day into a pretty day (i.e. 1 into 1st, 2 into 2nd, etc)
+	function dayConvert(day) {
+		day = parseInt(day);
+		
+		// just handle the cases for numbers 1-31
+		if (day===1 || day===21 || day===31) {
+			return day + 'st';
+		}
+		else if (day===2 || day===22) {
+			return day + 'nd';
+		}
+		else if (day===3 || day===23) {
+			return day + 'rd';
+		}
+		else {
+			return day + 'th';
+		}
+	}
+	
+	return return_array;
 }
 
 /* --- Debug ---
